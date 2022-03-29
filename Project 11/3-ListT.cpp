5423b5239218739de2b4c94c2451838c373fd6ab
Ian-Flury <ianflury123@gmail.com>
2022-03-28 20:49:34 -0700

    Getting a seg fault when calling the PutItemT function when the function is correct, (checked with cole).

--- Project 11/3-ListT.cpp
+++ Project 11/3-ListT.cpp
@@ -11,10 +11,32 @@ ListT::ListT()
 }
 
 ListT::~ListT()
-{}
+{
+    node* curNode = head;
+    //this loop will go through all of the nodes in the list,
+    //and preform the necessary operations to delete them.
+    while (curNode != nullptr)
+    {
+        node* nextNode = curNode->next;
+        delete curNode;
+        curNode = nextNode;
+    }
+    head = nullptr;
+    delete head;
+    tail = nullptr;
+    delete tail;
+}
 
 void ListT::PutItemH(itemType item)
 {
+    //to put an item at the head of the list, create a new node
+    //and assign the item of this node to the input item.
+    //splice the node into the front of the list.
+    node* temp = new node;
+    temp->item = item;
+    temp->next = head;
+    head = temp;
+    length++;
 }
 
 void ListT::Print() const
@@ -24,5 +46,120 @@ void ListT::Print() const
     {
         cout << cur->item << endl;
         cur = cur->next;
+    }
+}
+
+itemType ListT::GetItemH() const
+{
+    return head->item;
+}
+
+bool ListT::IsEmpty() const
+{
+    if (length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int ListT::GetLength() const
+{
+    return length;
+}
+
+void ListT::DeleteItemH()
+{
+    node* temp = head->next;
+    delete head;
+    head = temp;
+    length--;
+}
+
+int ListT::FindItem(const itemType target) const
+{
+    int numberOfNodesWithItem = 0;
+    //first make sure the list isn't empty.
+    if (length != 0) {
+        node *cur = head;
+
+        //loop through each node in the list.
+        while (cur != nullptr) {
+            if (cur->item == target) {
+                numberOfNodesWithItem++;
+            }
+            //make sure to update cur
+            cur = cur->next;
+        }
+    }
+    return numberOfNodesWithItem;
+}
+
+int ListT::DeleteItem(const itemType target)
+{
+    //first take care of the case when the head contains
+    //the target item. Loop in case the first few items
+    //in the list are all targets.
+    int numberNodesDeleted = 0;
+    while (head->item == target)
+    {
+        DeleteItemH();
+        numberNodesDeleted++;
+    }
+
+    //init the pointers that we will use to manipulate the
+    //list.
+    node* prev = head;
+    node* cur = head->next;
+
+    //at this point we know that the head of the
+    //list is not the target.
+    while (cur != nullptr)
+    {
+        //check if the current node's item is the target.
+        if (cur->item == target)
+        {
+            node* rmv = cur;
+            cur = cur->next;
+            prev->next = cur;
+            rmv->next = nullptr;
+            delete rmv;
+            length--;
+            numberNodesDeleted++;
+        } else {
+            prev = prev->next;
+            cur = cur->next;
+        }
+    }
+    return numberNodesDeleted;
+}
+
+itemType ListT::GetItemT() const
+{
+    return tail->item;
+}
+
+void ListT::PutItemT(const itemType itemIn)
+{
+    if (length == 0)
+    {
+        node* temp = new node;
+        temp->item = itemIn;
+        temp->next = nullptr;
+
+        head = temp;
+        tail = temp;
+
+        length++;
+    } else {
+        node* temp = new node;
+        temp->item = itemIn;
+
+        temp->next = nullptr;
+
+        tail->next = temp;
+        tail = temp;
+
+        length++;
     }
 }
--- Project 11/3-ListTMain.cpp
+++ Project 11/3-ListTMain.cpp
@@ -5,12 +5,211 @@ using namespace std;
 
 int main()
 {
- //Static list 
- ListT lst;
- for (int i = 0; i < 5; i++) 
-   lst.PutItemH(i);
- lst.Print();
+//Static list
+    ListT lst;
+    ListT* lst1 = new ListT;
 
- cout << endl;
- return 0;
+    //Test of PutItemH() & Print()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 PutItemH & Print()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: a list holding the numbers 0-4" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    lst.Print();
+
+    cout << "*****Test 2  PutItemH & Print()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: a list holding the numbers 0-4" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    lst1->Print();
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of PutItemH() & Print()
+//-------------------------------------------------------------------------------
+    //Test of IsEmpty()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 IsEmpty()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: 1 if empty, 0 if not." << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << lst.IsEmpty() << endl;
+
+    cout << "*****Test 2  IsEmpty()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: 1 if empty, 0 if not." << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << lst1->IsEmpty() << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of IsEmpty()
+//-------------------------------------------------------------------------------
+    //Test of GetLength()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 GetLength()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the length of the list" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << lst.GetLength() << endl;
+
+    cout << "*****Test 2  GetLength()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the length of the list" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << lst1->GetLength() << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of GetLength()
+//-------------------------------------------------------------------------------
+    //Test of GetItemH()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 GetItemH()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the item at the head of the list" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << lst.GetItemH() << endl;
+
+    cout << "*****Test 2  GetItemH()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the item at the head of the list" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << lst1->GetItemH() << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of GetItemH()
+//-------------------------------------------------------------------------------
+    //Test of DeleteItemH()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 DeleteItemH()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the new list without the old head item." << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << "before head item deletion:" << endl;
+    lst.Print();
+    lst.DeleteItemH();
+    cout << "after head item deletion:" << endl;
+    lst.Print();
+
+    cout << "*****Test 2  DeleteItemH()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the new list without the old head item." << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << "before head item deletion:" << endl;
+    lst1->Print();
+    lst1->DeleteItemH();
+    cout << "after head item deletion:" << endl;
+    lst1->Print();
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of DeleteItemH()
+//-------------------------------------------------------------------------------
+    //Test of Find()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 Find()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the number of nodes that store the target" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << lst.FindItem(4) << endl;
+
+    cout << "*****Test 2  Find()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the number of nodes that store the target" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << lst1->FindItem(4) << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of Find()
+//-------------------------------------------------------------------------------
+    //Test of DeleteItem()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 DeleteItem()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    cout << lst.DeleteItem(2) << endl;
+
+    cout << "*****Test 2  DeleteItem()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    cout << lst1->DeleteItem(2) << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of DeleteItem()
+//-------------------------------------------------------------------------------
+    //Test of PutItemT()
+    /*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 PutItemT()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    lst.Print();
+    cout << "list after insert at tail:" << endl;
+    lst.PutItemT(2);
+    lst.Print();
+
+    cout << "*****Test 2  PutItemT()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    lst1->Print();
+    cout << endl;
+    lst1->PutItemT(2);
+    lst1->Print();
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of PutItemT()
+//-------------------------------------------------------------------------------
+    //Test of PutItemT()
+    ///*
+    cout << "***************************************" << endl;
+    cout << "*****Test 1 PutItemT()*****" << endl;
+    cout << "static test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst.PutItemH(i);
+    lst.Print();
+    cout << "item: " << endl;
+    cout << lst.GetItemT() << endl;
+
+    cout << "*****Test 2  PutItemT()*****" << endl;
+    cout << "dynamic test" << endl;
+    cout << "output: the number of nodes deleted" << endl;
+    for (int i = 0; i < 5; i++)
+        lst1->PutItemH(i);
+    lst1->Print();
+    cout << lst1->GetItemT() << endl;
+    cout << "***************************************" << endl;
+    //*/
+    //End Test of PutItemT()
+//-------------------------------------------------------------------------------
+
+
+    delete lst1; //necessary to invoke destructor on dynamic list
+    cout << endl;
+
+    return 0;
 }
