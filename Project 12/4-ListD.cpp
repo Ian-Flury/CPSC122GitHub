3d72e217f878d090c589749ede27adea771b0c87
Ian-Flury <ianflury123@gmail.com>
2022-04-03 13:09:52 -0700

    project finished.

--- Project 12/4-ListD.cpp
+++ Project 12/4-ListD.cpp
@@ -1,53 +1,75 @@
+/* Name: Ian Flury
+ * Class: CPSC 122, Section 02
+ * Assignment: Project 12 Doubly Linked List
+ * Date Submitted: 4/3/2022
+ * Notes: See the header and test files.
+ */
 #include <iostream>
 #include <climits>
 using namespace std;
 
 #include "4-ListD.h"
 
-ListD::ListD()
+//Constructor
+template <typename T>
+ListD<T>::ListD()
 {
- InitializeVars();
+    InitializeVars();
 }
 
-ListD::ListD(ListD* lst)
+//Copy Constructor
+//Creates an exact copy of a pre-existing list.
+template <typename T>
+ListD<T>::ListD(ListD* lst)
 {
- InitializeVars();
- //returns pointer to the first node, which is what we want here
- doubleNode* cur = lst->FindPosition(2); //enter 1 more than u want to find.
- for (int i = 1; i <= lst->length; i++)
- {
-  cout << cur->item << endl;
-  Insert(cur->item,i);
-  cur = cur->next;
- }
+    InitializeVars();
+    //returns pointer to the first node, which is what we want here
+    doubleNode<T>* cur = lst->FindPosition(2); //enter 1 more than u want to find.
+    for (int i = 1; i <= lst->length; i++)
+    {
+        cout << cur->item << endl;
+        Insert(cur->item,i);
+        cur = cur->next;
+    }
 }
 
-void ListD::InitializeVars()
+/* Description: This function initializes the head and tail
+ *              dummy nodes.
+ * Inputs: none.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::InitializeVars()
 {
- length = 0;
- 
+    length = 0;
+
 //create dummy nodes;
- head = new doubleNode;
- tail = new doubleNode;
+    head = new doubleNode<T>;
+    tail = new doubleNode<T>;
 
- //set values for head dummy node;
- head->prev = NULL;
- head->item = INT_MIN;
- head->next = tail;
+    //set values for head dummy node;
+    head->prev = NULL;
+    //head->item = INT_MIN;
+    head->next = tail;
 
- //set values for tail dummy node;
- tail->prev = head; 
- tail->item = INT_MAX;
- tail->next = NULL;
+    //set values for tail dummy node;
+    tail->prev = head;
+    //tail->item = INT_MAX;
+    tail->next = NULL;
 }
 
-//TODO: will I lose points for not using the Delete function
-//  in the Destructor? (once finished, redo the constructor to use
-//  the Delete function).
-ListD::~ListD()
+/* Description: This function tears down the list and
+ *              re-allocates any used memory.
+ * Inputs: none.
+ * Outputs: none.
+ */
+template <typename T>
+ListD<T>::~ListD()
 {
+    //while there are still nodes, keep deleting
+    //nodes.
     while (head != nullptr) {
-        doubleNode* cur = head;
+        doubleNode<T>* cur = head;
         head = head->next;
         cur->next = nullptr;
         cur->prev = nullptr;
@@ -57,55 +79,99 @@ ListD::~ListD()
     tail = nullptr;
 }
 
-doubleNode* ListD::FindPosition(int pos)
+/* Description: This function give a pointer to the node before
+ *              the position given as an input. It is used in
+ *              Insert() and Delete().
+ *
+ * Inputs: int pos, the position of the desired node in the list.
+ * Outputs: a pointer to the node before the node at the input position.
+ */
+template <typename T>
+doubleNode<T>* ListD<T>::FindPosition(int pos)
 {
- //Inserting at the tail is a special case.  It can be made much more efficient than
- //this.
- doubleNode* cur = head;
- int i = 0;  //begin at the dummy node
- while (i < pos - 1)
- {
-  cur = cur->next;
-  i++;
- }
- return cur;
-} 
-  
-void ListD::Insert(itemType item, int pos)
-{
- //new node goes between these two nodes
- doubleNode* insertPtA = FindPosition(pos);  
- doubleNode* insertPtB = insertPtA->next; 
-
- //create new node and set its values
- doubleNode* tmp = new doubleNode; 
- tmp->prev = insertPtA;
- tmp->item = item;
- tmp->next = insertPtB;
-
- //set pointers for nodes before and after the insertion point
- insertPtA->next = tmp;
- insertPtB->prev = tmp;
-
- length++;
+    //Inserting at the tail is a special case.  It can be made much more efficient than
+    //this.
+    doubleNode<T>* cur = head;
+    int i = 0;  //begin at the dummy node
+    while (i < pos - 1)
+    {
+        cur = cur->next;
+        i++;
+    }
+    return cur;
 }
 
-void ListD::PrintForward()
+/* Description: This function puts new items into the list.
+ *
+ * Inputs: int pos, the position of where the item is to be inserted.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::Insert(T item, int pos)
 {
- doubleNode*  cur = head->next;
+    //when putting things at the end of the list, just use
+    //the fact that we have a tail pointer to avoid traversing
+    //the list every time you want to put a new item at the end.
+    //this makes the operation orders of magnitude faster when
+    //inserting at the end of a large list.
+    if (pos == length + 1)
+    {
+        doubleNode<T>* temp = new doubleNode<T>;
+        temp->item = item;
+        doubleNode<T>* tailPrev = tail->prev;
 
- int i = 0;
- while (i < length)
- {
-  cout << cur->item << endl;
-  cur = cur->next;
-  i++;
- }
+        tailPrev->next = temp;
+        temp->prev = tailPrev;
+
+        temp->next = tail;
+        tail->prev = temp;
+
+        length++;
+    } else {
+        //new node goes between these two nodes
+        doubleNode<T> *insertPtA = FindPosition(pos);
+        doubleNode<T> *insertPtB = insertPtA->next;
+
+        //create new node and set its values
+        doubleNode<T> *tmp = new doubleNode<T>;
+        tmp->prev = insertPtA;
+        tmp->item = item;
+        tmp->next = insertPtB;
+
+        //set pointers for nodes before and after the insertion point
+        insertPtA->next = tmp;
+        insertPtB->prev = tmp;
+        length++;
+    }
 }
 
-void ListD::PrintBackward()
+/* Description: This function prints the list, first element first.
+ *
+ * Inputs: none.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::PrintForward()
 {
-    doubleNode* cur = tail->prev;
+    doubleNode<T>*  cur = head->next;
+
+    int i = 0;
+    while (i < length)
+    {
+        cout << cur->item << endl;
+        cur = cur->next;
+        i++;
+    }
+}
+
+/* Description: This function prints the list last item first.
+ * Inputs: none.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::PrintBackward()
+{
+    doubleNode<T>* cur = tail->prev;
     int i = 0;
     while (i < length)
     {
@@ -115,12 +181,18 @@ void ListD::PrintBackward()
     }
 }
 
-void ListD::Delete(int pos)
+/* Description: This function deletes the item at a given position in the list.
+ *
+ * Inputs: int pos, the position of the node you want to delete.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::Delete(int pos)
 {
     if (length != 0) {
-        doubleNode *rmv = FindPosition(pos+1);
-        doubleNode *nextNode = rmv->next;
-        doubleNode *prevNode = rmv->prev;
+        doubleNode<T> *rmv = FindPosition(pos+1);
+        doubleNode<T> *nextNode = rmv->next;
+        doubleNode<T> *prevNode = rmv->prev;
 
         nextNode->prev = prevNode;
         prevNode->next = nextNode;
@@ -133,21 +205,23 @@ void ListD::Delete(int pos)
     }
 }
 
-//You don't have to deal with the case where you have
-//to loop through multiple items at the head of the list.
-//don't use the Delete() function because it makes things
-//slower.
-int ListD::DeleteAll(itemType item)
+/* Description: This function give deletes all the instances of
+ *              a specific item in the list.
+ * Inputs: T item, the item you want to remove from the list.
+ * Outputs: int, the number of nodes deleted from the list.
+ */
+template <typename T>
+int ListD<T>::DeleteAll(T item)
 {
     int numberNodesDeleted = 0;
-    doubleNode* prv = head;
-    doubleNode* cur = head->next;
+    doubleNode<T>* prv = head;
+    doubleNode<T>* cur = head->next;
 
     while (cur != nullptr)
     {
         if (cur->item == item)
         {
-            doubleNode* rmv = cur;
+            doubleNode<T>* rmv = cur;
             cur = cur->next;
             cur->prev = prv;
             prv->next = cur;
@@ -165,25 +239,33 @@ int ListD::DeleteAll(itemType item)
     return numberNodesDeleted;
 }
 
-//Use Selection Sort;
-void ListD::Sort()
+/* Description: This function give uses the selection sort
+ *              algorithm to sort the list in ascending order.
+ * Inputs: none.
+ * Outputs: none.
+ */
+template <typename T>
+void ListD<T>::Sort()
 {
-    doubleNode* cur = head;
+    doubleNode<T>* cur = head;
 
     //traversing the whole list
     while (cur != nullptr)
     {
-        doubleNode* currentMin = cur;
-        doubleNode* cur2 = cur->next;
+        doubleNode<T>* currentMin = cur;
+        doubleNode<T>* cur2 = cur->next;
         //traversing the unsorted region of the list
         while (cur2 != nullptr) {
+            //if you find an item that is smaller than the current min,
+            //set that node at the current min.
             if (currentMin->item > cur2->item) {
                 currentMin = cur2;
             }
             cur2 = cur2->next;
         }
 
-        itemType temp = cur->item;
+        //do the item swap.
+        T temp = cur->item;
         cur->item = currentMin->item;
         currentMin->item = temp;
         cur = cur->next;
--- Project 12/4-ListDTst.cpp
+++ Project 12/4-ListDTst.cpp
@@ -1,21 +1,111 @@
+/* Name: Ian Flury
+ * Class: CPSC 122, Section 02
+ * Assignment: Project 12 Doubly Linked List
+ * Date Submitted: 4/3/2022
+ * Notes: See the header and implementation files.
+ */
 #include <iostream>
 using namespace std;
 
-#include "4-ListD.h"
+#include "4-ListD.cpp"
 
 void TestInsert();
 void TestCopyConstructor();
+void TestPrintBackward();
+void TestDelete();
+void TestDeleteAll();
+void TestSort();
+void TestType();
 
 int main()
 {
- //TestInsert();
- TestCopyConstructor();
+    TestInsert();
+    //TestCopyConstructor();
+    //TestPrintBackward();
+    //TestDelete();
+    //TestDeleteAll();
+    //TestSort();
+    //TestType();
+
+    return 0;
+}
+
+void TestType()
+{
+    cout << "This test will fill the list with "
+            "a different type: (string)" << endl;
+    ListD<string> lst;
+    lst.Insert("Hello",1);
+    lst.Insert("World",2);
+    lst.PrintForward();
+}
+
+void TestSort()
+{
+    ListD<int> lst;
+    lst.Insert(3,1);
+    lst.Insert(4,1);
+    lst.Insert(23,1);
+    lst.Insert(1,1);
+    lst.Insert(8,1);
+    lst.Insert(56,1);
+    lst.Insert(9,1);
+    cout << "The unsorted list:" << endl;
+    lst.PrintForward();
+    cout << endl;
+    cout << "The (selection) sorted list:" << endl;
+    lst.Sort();
+    lst.PrintForward();
+}
+
+void TestDeleteAll()
+{
+    ListD<int> lst;
+    lst.Insert(2,1);
+    lst.Insert(2,2);
+    lst.Insert(4,3);
+    lst.Insert(2,4);
+    lst.Insert(3,5);
+    lst.Insert(2,6);
+    lst.PrintForward();
+    int nodeDel = lst.DeleteAll(2);
+    cout << "after delete: " << endl;
+    lst.PrintForward();
+    cout << endl << "there were " << nodeDel << " nodes deleted" << endl;
+}
+
+void TestDelete()
+{
+    ListD<int> lst;
+    for (int i = 1; i <= 6; i++)
+    {
+        lst.Insert(i,i);
+    }
+    cout << "The list before deletion:" << endl;
+    lst.PrintForward();
+    cout << "The list after deleting item 3:" << endl;
+    lst.Delete(3);
+    lst.PrintForward();
+}
+
+void TestPrintBackward()
+{
+    ListD<int> lst;
+    cout << "here is the list 'printed forward':" << endl;
+    for (int i = 1; i <= 5; i++)
+    {
+        lst.Insert(i,i);
+    }
+    lst.PrintForward();
+
+    cout << "here is the list 'printed backward':" << endl;
+    lst.PrintBackward();
 }
 
 void TestInsert()
 {
 
- ListD* lst = new ListD;
+ ListD<int>* lst = new ListD<int>;
  for (int i = 1; i <= 5; i++)
   lst->Insert(i,i);
  lst->PrintForward();
@@ -49,19 +139,16 @@ void TestInsert()
 
 void TestCopyConstructor()
 {
- ListD* lst1 = new ListD;
+ ListD<int>* lst1 = new ListD<int>;
  for (int i = 1; i <= 3; i++)
   lst1->Insert(i,i);
 
- ListD* lst2(lst1);
+ ListD<int>* lst2(lst1);
  
  cout << "Traverse lst1" << endl;
  lst1->PrintForward();
  cout << endl;
  cout << "Traverse lst2" << endl;
  lst2->PrintForward();
-
-
- //delete lst1;
- //delete lst2;
+ delete lst1;
 }
