85290ef0702b0ace04cfb5076d5f95fae22166d3
Ian-Flury <ianflury123@gmail.com>
2022-04-29 09:44:36 -0700

    full functionality

--- EC3 Project/queueP.cpp
+++ EC3 Project/queueP.cpp
@@ -6,15 +6,6 @@ using namespace std;
 PQueue::PQueue() : QueueI()
 {
     cout << "constructor priority queue" << endl;
-    Enqueue(55);
-    Enqueue(1);
-    Enqueue(36);
-    Enqueue(54);
-    Enqueue(14);
-    Enqueue(32);
-    Print();
-    cout << "peeked: " << Peek() << endl;
-    Print();
 }
 
 PQueue::PQueue(PQueue* q) : QueueI(q)
@@ -26,12 +17,16 @@ void PQueue::Enqueue(itemType newItem)
     if (length == 0) {
         QueueI::Enqueue(newItem);
     } else {
-        node* nodeJBeforeInsert = PtrTo(newItem);
-        node* temp = new node;
-        temp->next = nodeJBeforeInsert->next;
-        nodeJBeforeInsert->next = temp;
-        temp->item = newItem;
-        length++;
+        if (head->item > newItem) {
+            PutItemH(newItem);
+        } else {
+            node* follower = PtrTo(newItem);
+            node* newNode = new node;
+            newNode->item = newItem;
+            newNode->next = follower->next;
+            follower->next = newNode;
+            length++;
+        }
     }
 }
 
@@ -40,23 +35,20 @@ void PQueue::Enqueue(itemType newItem)
 //same priority.
 node* PQueue::PtrTo(itemType newItem)
 {
-    cout << "funct reached" << endl;
     node* follower = head;
     node* target = head;
-    if (head->item > newItem) {
-        return head;
-    }
 
     while (true) {
-        if (target == nullptr) {
-            return tail;
-        }
         target = target->next;
-        if (target->item > newItem) {
+        if (target == nullptr) {
+            target = tail;
+            return target;
+        } else if (target->item > newItem) {
             return follower;
         } else {
             follower = follower->next;
         }
     }
+
 }
 	
--- EC3 Project/queuePTst.cpp
+++ EC3 Project/queuePTst.cpp
@@ -7,14 +7,27 @@ using namespace std;
 int main()
 {
 
- PQueue* que = new PQueue;
- /*
- for (int i = 5; i >= 0; i--)
-  que->Enqueue(i);
- que->Dequeue();
- que->Print();
- cout << que->Peek() << endl;
- */
- delete que;
+    PQueue* que = new PQueue;
+    for (int i = 5; i >= 0; i--)
+        que->Enqueue(i);
+    que->Print();
+    cout << endl << endl;
+    que->Dequeue();
+    que->Print();
+    cout << "Peaked: " << que->Peek() << endl;
+
+    cout << endl << endl;
+
+    que->Enqueue(69);
+    que->Print();
+    cout << endl << endl;
+
+    que->Enqueue(23);
+    que->Print();
+    que->Dequeue();
+    cout << endl << endl;
+    que->Print();
+
+    delete que;
  
 }
