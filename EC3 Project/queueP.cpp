bcc927a0911b90006aef8ff73713f67b5519f67c
Ian-Flury <ianflury123@gmail.com>
2022-04-29 15:41:11 -0700

    added comments

--- EC3 Project/queueP.cpp
+++ EC3 Project/queueP.cpp
@@ -12,11 +12,20 @@ PQueue::PQueue(PQueue* q) : QueueI(q)
 {
 }
 
+/* Description: This function puts adds a new item in the queue in
+ *              sorted order (priority).
+ * Inputs: itemtype newItem, the item that the user wants to add.
+ * Outputs: none.
+ */
 void PQueue::Enqueue(itemType newItem)
 {
+    //if the length is 0, we can just Enqueue normally
+    //just like we did in QueueI.
     if (length == 0) {
         QueueI::Enqueue(newItem);
     } else {
+        //first check if the item at the head is of lower
+        //priority than the newItem. If it is, insert at head.
         if (head->item > newItem) {
             PutItemH(newItem);
         } else {
@@ -30,11 +39,19 @@ void PQueue::Enqueue(itemType newItem)
     }
 }
 
-//will return a pointer to the node just before the
-//node with a higher or same value. This means lower or
-//same priority.
+/* Description: This function is used by Enqueue and returns a pointer
+ *              to the node just before the place where the new node is
+ *              to be inserted.
+ * Inputs: itemtype newItem, the item that the user wants to add.
+ * Outputs: node* follower, a pointer to the node where the new node will
+ *          be inserted by Enqueue.
+ */
 node* PQueue::PtrTo(itemType newItem)
 {
+    //in this function, node* follower is just
+    //behind target. When target->item is of lower priority
+    //than newItem, follower (one node behind) will be
+    //returned and the new node inserted just after.
     node* follower = head;
     node* target = head;
 
@@ -49,6 +66,5 @@ node* PQueue::PtrTo(itemType newItem)
             follower = follower->next;
         }
     }
-
 }
 	
